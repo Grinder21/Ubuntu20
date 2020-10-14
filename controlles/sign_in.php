b1c750694957e5e92a8d84db33390d1aa7ab395a
Grinder21 <spartawinner280@mail.ru>
2020-10-14 14:51:20 +0300

    restart_1

--- controlles/sign_in.php
+++ controlles/sign_in.php
@@ -2,4 +2,5 @@
 	<p>Login succesful</p>
 	<?php } else { ?>
 	<p>Error login</p>
+	<a href="/view/sign_in.php">Restart login</a>
 	<?php } ?> 
--- controlles/sign_up.php
+++ controlles/sign_up.php
@@ -1,6 +1,14 @@
-
-<?php if (isset($_POST['regform'])) { ?>
-	<p>Register succesful</p>
-	<?php } else { ?>
-	<p>Error registration</p>
-	<?php } ?> 
+<?php
+$name = htmlspecialchars(stripslashes($_POST['name']));
+$password = htmlspecialchars(stripslashes($_POST['password']));
+$email = htmlspecialchars(stripslashes($_POST['email'])); 
+if (preg_match("/^[0-9a-z_\.]+@[0-9a-z_^\.]+\.[a-z]{2,6}$/i", $mail) && isset($_POST['regform']) && !empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['password'])) {
+	echo 'Вы зарегистрированы!';
+	$fp = fopen('users.txt', 'w');
+	fwrite($fp, "$name, $password, $email");
+	fclose($fp);
+} else {
+	echo 'Произошла ошибка регистрации';
+	header('Location: http://localhost/');
+}	
+?>

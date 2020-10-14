59938a7fb7a63fb3124d3849a811d23bdc98d000
Grinder21 <spartawinner280@mail.ru>
2020-10-14 12:28:27 +0300

    new files

--- index.php
+++ index.php
@@ -1,3 +1,45 @@
 <?php
-echo 'Hello world';
+//define('DB_HOST', 'localhost'); // const 
+//define('DB_USER', 'root'); // const
+//define('DB_PASSWORD', 'password'); // const
+//define('DB_NAME', 'mysite'); // const
+
+$mysqli = $new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+if ($mysqli->maxdb_connect_errno) exit ('Error connection datebase');
+$mysqli->set_charset('utf8');
+
+if (isset($_POST['reg'])) {
+	$name = $mysqli->real_escape_string(htmlspecialchars($_POST['name']));
+	$email = $mysqli->real_escape_string(htmlspecialchars($_POST['email']));
+	$password = $mysqli->real_escape_string(htmlspecialchars($_POST['password']));
+	$ip_reg = ip2long($_SERVER['REMOTE_ADDR']);
+	$query = "INSERT INTO `form_registration`(`name`, `email`, `password`, `ip_reg`, `date_reg`)
+	VALUES('$name', '$email', 'MD5($password)', '$ip_reg', UNIX_TIMESTAMP())";
+	$result = $mysqli->query($query);
+}
+
+$mysqli->close();
+
 ?>
+<?php if(isset($result)) { ?>
+	<?php if($result) ?>
+	<p>Register success</p>
+	<?php } else { ?>
+		<p>Error registration!</p>
+		<?php } ?>
+
+<?php } ?>
+<form name="reg" action="index.php" method="post">
+	<p>
+	Name: <input type="text" name="name">
+	</p>
+	<p>
+	E-mail: <input type="email" name="email">
+	</p>
+	<p>
+	Password: <input type="password" name="password">
+	</p>
+	<p>
+	<input type="submit" name="sendform" value='Send form'>
+	</p>
+</form>

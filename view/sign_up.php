9c43253f3b2720784d7b37bbd89394ae12891f11
Grinder21 <spartawinner280@mail.ru>
2020-10-14 12:42:48 +0300

    new responce

--- view/sign_up.php
+++ view/sign_up.php
@@ -6,30 +6,13 @@
 
 //$mysqli = $new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 //if ($mysqli->maxdb_connect_errno) exit ('Error connection datebase');
-$mysqli->set_charset('utf8');
+//$mysqli->set_charset('utf8');
 
 if (isset($_POST['reg'])) {
-	$name = $mysqli->real_escape_string(htmlspecialchars($_POST['name']));
-	$email = $mysqli->real_escape_string(htmlspecialchars($_POST['email']));
-	$password = $mysqli->real_escape_string(htmlspecialchars($_POST['password']));
-	$ip_reg = ip2long($_SERVER['REMOTE_ADDR']);
-	$query = "INSERT INTO `form_registration`(`name`, `email`, `password`, `ip_reg`, `date_reg`)
-	VALUES('$name', '$email', 'MD5($password)', '$ip_reg', UNIX_TIMESTAMP())";
-	$result = $mysqli->query($query);
+ echo 'Thanks for registration';
 }
-
-$mysqli->close();
-
 ?>
-<?php if(isset($result)) { ?>
-	<?php if($result) ?>
-	<p>Register success</p>
-	<?php } else { ?>
-		<p>Error registration!</p>
-		<?php } ?>
-
-<?php } ?>
-<form name="reg" action="index.php" method="post">
+<form name="reg" action="sign_in.php" method="post">
 	<h3>Registration user</h3>
 	<p>
 	Name: <input type="text" name="name">

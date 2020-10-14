93d37a4d37cf6fcaafc4822a208b2b50d36e5794
Grinder21 <spartawinner280@mail.ru>
2020-10-14 15:21:04 +0300

    finally112121

--- controlles/sign_up.php
+++ controlles/sign_up.php
@@ -9,7 +9,7 @@ $email = htmlspecialchars(stripslashes($_POST['email']));
 	<?php header('Refresh: 10; url=http://localhost/index.php'); ?>
 <?php } ?>
 
-<?php if (!preg_match("/^[0-9a-z_\.]+@[0-9a-z_^\.]+\.[a-z]{2,6}$/i", $mail)) { ?>
+<?php if (!preg_match("/^[0-9a-z_\.]+@[0-9a-z_^\.]+\.[a-z]{2,6}$/i", $email)) { ?>
 	<p>Вы ввели некорректный email-адрес</p>
 	<?php header('Refresh: 1; url=http://localhost/view/sign_up.php'); ?>
 <?php } ?>

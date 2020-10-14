ba927433224f013b6583c72d659760d255ba93de
Grinder21 <spartawinner280@mail.ru>
2020-10-14 16:37:16 +0300

    new

--- controlles/sign_up.php
+++ controlles/sign_up.php
@@ -2,12 +2,12 @@
 $name = htmlspecialchars(stripslashes($_POST['name']));
 $password = htmlspecialchars(stripslashes($_POST['password']));
 $email = htmlspecialchars(stripslashes($_POST['email']));
-require_once('../base.php');
 ?>
 
 <?php if (isset($_POST['regform'])) { ?>
 	<p>Данные с формы заполнены и пришли успешно!</p>
-	<?php header('Refresh: 10; url=http://localhost/index.php'); ?>
+	<?php header('Refresh: 10; url=http://localhost/index.php');
+	require_once('../base.php'); ?>
 <?php } ?>
 
 <?php if (!preg_match("/^[0-9a-z_\.]+@[0-9a-z_^\.]+\.[a-z]{2,6}$/i", $email)) { ?>

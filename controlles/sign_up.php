edc43964114a5771239affbd94f778d19eca4c9f
Grinder21 <spartawinner280@mail.ru>
2020-10-14 15:12:06 +0300

    finally

--- controlles/sign_up.php
+++ controlles/sign_up.php
@@ -4,9 +4,17 @@ $password = htmlspecialchars(stripslashes($_POST['password']));
 $email = htmlspecialchars(stripslashes($_POST['email']));
 ?>
 
-<?php if (isset($_POST['regform'])) { ?>
-	<p>Форма пришла</p>
+<?php if (isset($_POST['regform']) && (!empty($_POST['name'])) && (!empty($_POST['password'])) && (!empty($_POST['email']))) { ?>
+	<p>Данные с формы заполнены и пришли успешно!</p>
 <?php } ?>
+
+<?php if (!preg_match("/^[0-9a-z_\.]+@[0-9a-z_^\.]+\.[a-z]{2,6}$/i", $mail)) { ?>
+	<p>Вы ввели некорректный email-адрес</p>
+	<?php header('Location: localhost/view/sign_up.php') ?>
+<?php } ?>
+
+
+
 
 
 
